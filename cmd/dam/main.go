9c0089a0becc88ac5db3251ba8dfd85ff52cd934
VictorFrank <vfrankbr@gmail.com>
2025-03-09 12:04:41 +0100

    Basic REST API created - Struct for image request added

--- cmd/dam/main.go
+++ cmd/dam/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ImageReq struct {
+	ImgID     string `form:"img_id" binding:"omitempty"`
+	ObsID     string `form:"obs_id" binding:"omitempty"`
+	StartTime string `form:"start_time" binding:"omitempty"`
+	EndTime   string `form:"end_time" binding:"omitempty"`
+	LatFrom   string `form:"lat_from" binding:"omitempty"`
+	LatTo     string `form:"lat_to" binding:"omitempty"`
+	LonFrom   string `form:"lon_from" binding:"omitempty"`
+	LonTo     string `form:"lon_to" binding:"omitempty"`
+	CamType   string `form:"cam_type" binding:"required"`
+	Date      string `form:"date" binding:"omitempty"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	g := gin.Default()
+
+	g.GET("/search-images", func(context *gin.Context) {
+		var req ImageReq
+		if err := context.ShouldBindQuery(&req); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"message":  "Success",
+			"recieved": req,
+		})
+	})
+	g.Run(":8081")
 }
