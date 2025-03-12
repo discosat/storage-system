f040be1029defeaab50eb357a2f58dbeb54f5932
VictorFrank <vfrankbr@gmail.com>
2025-03-12 11:32:37 +0100

    extracts ReqReturn func from start func within dam - Creates test http file

--- cmd/dam/dam.go
+++ cmd/dam/dam.go
@@ -2,36 +2,45 @@ package dam
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"time"
 )
 
 type ImageReq struct {
-	ImgID     string `form:"img_id" binding:"omitempty"`
-	ObsID     string `form:"obs_id" binding:"omitempty"`
-	StartTime string `form:"start_time" binding:"omitempty"`
-	EndTime   string `form:"end_time" binding:"omitempty"`
-	LatFrom   string `form:"lat_from" binding:"omitempty"`
-	LatTo     string `form:"lat_to" binding:"omitempty"`
-	LonFrom   string `form:"lon_from" binding:"omitempty"`
-	LonTo     string `form:"lon_to" binding:"omitempty"`
-	CamType   string `form:"cam_type" binding:"required"`
-	Date      string `form:"date" binding:"omitempty"`
+	ImgID     *string    `form:"img_id" binding:"omitempty"`
+	ObsID     *string    `form:"obs_id" binding:"omitempty"`
+	StartTime *time.Time `form:"start_time" binding:"omitempty"`
+	EndTime   *time.Time `form:"end_time" binding:"omitempty"`
+	LatFrom   *float64   `form:"lat_from" binding:"omitempty"`
+	LatTo     *float64   `form:"lat_to" binding:"omitempty"`
+	LonFrom   *float64   `form:"lon_from" binding:"omitempty"`
+	LonTo     *float64   `form:"lon_to" binding:"omitempty"`
+	CamType   string     `form:"cam_type" binding:"required"`
+	Date      *time.Time `form:"date" binding:"omitempty"`
 }
 
 func Start() {
 	g := gin.Default()
 
-	g.GET("/search-images", func(context *gin.Context) {
-		var req ImageReq
-		if err := context.ShouldBindQuery(&req); err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	g.GET("/search-images", ReqReturn)
 
-		context.JSON(http.StatusOK, gin.H{
-			"message":  "Success",
-			"recieved": req,
-		})
+	err := g.Run(":8081")
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+}
+
+func ReqReturn(c *gin.Context) {
+	var req ImageReq
+
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "Success",
+		"recieved": req,
 	})
-	g.Run(":8081")
 }
