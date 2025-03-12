8b66257eedeab8475168d3211f45ee8c10afecd8
VictorFrank <vfrankbr@gmail.com>
2025-03-12 12:00:21 +0100

    removes  comment on go dam.Start()

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/discosat/storage-system/cmd/dam"
 	"github.com/discosat/storage-system/cmd/dim"
 	"log"
 )
@@ -8,7 +9,7 @@ import (
 func main() {
 	log.Println("starting DIM-DAM system backend")
 
-	//go dam.Start()
+	go dam.Start()
 	go dim.Start()
 
 	select {}
