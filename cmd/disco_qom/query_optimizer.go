92b7317918bbf42762cac0f43410ddd4b1595bbe
VictorFrank <vfrankbr@gmail.com>
2025-03-28 14:59:41 +0100

    Adds functionality to take out values from Image request

--- cmd/disco_qom/query_optimizer.go
+++ cmd/disco_qom/query_optimizer.go
@@ -8,14 +8,21 @@ import (
 type DiscoQO struct{}
 
 func (q *DiscoQO) Optimize(query interfaces.ImageRequest) error {
+	//sqlQuery, args := StringToSQLTranslator(query)
+	StringToSQLTranslator(query)
 
 	return nil
 }
 
-func StringToSQLTranslator(queryArray []string) {
-	for v := range queryArray {
-		fmt.Println("Query String: ", queryArray[v])
+func StringToSQLTranslator(query interfaces.ImageRequest) (string, []interface{}) {
+	/*var conditions []string
+	var args []interface{}
+	argIndex := 1*/
+
+	if query.ImgID != nil {
+		fmt.Println("Logging image ID after stripping it from image request", *query.ImgID)
 	}
+	return "", nil
 }
 
 //var _ interfaces.QueryOptimizer = (*DiscoQO)(nil)
