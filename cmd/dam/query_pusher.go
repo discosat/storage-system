1630f141aad1cb6e941b1c06e979c04c966a262f
VictorFrank <vfrankbr@gmail.com>
2025-03-28 13:00:11 +0100

    Removes redundant functions

--- cmd/dam/query_pusher.go
+++ cmd/dam/query_pusher.go
@@ -9,52 +9,6 @@ type QueryParser struct {
 	qom interfaces.QueryOptimizer
 }
 
-/*func NewQueryParser(optimizer interfaces.QueryOptimizer) *QueryParser {
-	return &QueryParser{optimizer: optimizer}
-}
-
-func (qp *QueryParser) PushQuery(query map[string]interface{}) error {
-	stringQuery := ""
-
-	log.Println("DAM query logged in query_parser: ", query)
-
-	for key, val := range query {
-
-		if val == nil {
-			continue
-		}
-
-		switch v := val.(type) {
-		case string:
-			stringQuery += key + ":" + v + ","
-		case float64:
-			stringQuery += key + ":" + fmt.Sprintf("%f", v) + ","
-		case int64:
-			stringQuery += key + ":" + fmt.Sprintf("%d", v) + ","
-		case *string:
-			if v != nil {
-				stringQuery += key + ":" + fmt.Sprintf(*v) + ","
-			}
-		case *float64:
-			if v != nil {
-				stringQuery += key + ":" + fmt.Sprintf("%f", *v) + ","
-			}
-		case *int64:
-			if v != nil {
-				stringQuery += key + ":" + fmt.Sprintf("%d", *v) + ","
-			}
-		default:
-			log.Fatal("Unsupported type in query", v)
-		}
-	}
-
-	if len(stringQuery) > 0 {
-		stringQuery = stringQuery[:len(stringQuery)-2]
-	}
-
-	return qp.optimizer.Optimize(stringQuery)
-}*/
-
 func newQueryPusher(qom interfaces.QueryOptimizer) *QueryParser {
 	return &QueryParser{qom: qom}
 }
