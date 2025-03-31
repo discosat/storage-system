c128c50ae36e29c423261d872adcb957de264ee5
VictorFrank <vfrankbr@gmail.com>
2025-03-31 12:38:01 +0200

    Adds functionality to create and SQL query from GET request url input

--- cmd/disco_qom/query_optimizer.go
+++ cmd/disco_qom/query_optimizer.go
@@ -3,6 +3,7 @@ package disco_qom
 import (
 	"fmt"
 	"github.com/discosat/storage-system/cmd/interfaces"
+	"strings"
 )
 
 type DiscoQO struct{}
@@ -15,13 +16,29 @@ func (q *DiscoQO) Optimize(query interfaces.ImageRequest) error {
 }
 
 func StringToSQLTranslator(query interfaces.ImageRequest) (string, []interface{}) {
-	/*var conditions []string
-	var args []interface{}
+	var conditions []string
+	/*var args []interface{}
 	argIndex := 1*/
 
+	//Creates an SQL query based on the query string
+	//Is under risk of SQL injection - Fix using placeholders
 	if query.ImgID != nil {
 		fmt.Println("Logging image ID after stripping it from image request", *query.ImgID)
+		conditions = append(conditions, fmt.Sprintf("img_id = %s", *query.ImgID))
 	}
+
+	if query.CamType != nil {
+		fmt.Println("Logging cam type after stripping it from image request", *query.CamType)
+		conditions = append(conditions, fmt.Sprintf("cam_type = %s", *query.CamType))
+	}
+
+	sqlQuery := "SELECT * FROM images"
+	if len(conditions) > 0 {
+		sqlQuery += " WHERE " + strings.Join(conditions, " AND ")
+	}
+
+	fmt.Println("Logging sqlQuery: ", sqlQuery)
+
 	return "", nil
 }
 
