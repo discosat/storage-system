79231c9ba8688a8dfbdca7178b5effb9501e0446
VictorFrank <vfrankbr@gmail.com>
2025-04-02 11:41:42 +0200

    Comments out dim while working on dam

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/discosat/storage-system/cmd/dam"
-	"github.com/discosat/storage-system/cmd/dim"
 )
 
 func main() {
 	fmt.Println("starting DIM-DAM system backend")
 
 	go dam.Start()
-	go dim.Start()
+	//go dim.Start()
 
 	fmt.Println("DIM-DAM up and running")
 
