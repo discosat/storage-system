01e2345aa71d9285078d248db821709a78a4fb79
hannehue <hannehue@gmail.com>
2025-04-08 13:33:43 +0200

    Transaction work

--- cmd/dim/generic_object_store.go
+++ cmd/dim/generic_object_store.go
@@ -10,6 +10,7 @@ type IDataStore interface {
 	SaveFile(fileInfo *zip.File, openFile io.ReadCloser, bucketName string) (string, error)
 	SaveImage(fileHeader *multipart.FileHeader, openFile io.ReadCloser, bucketName string, observationName string) (string, error)
 	BucketExists(bucketName string) (bool, error)
+	DeleteImage(imgRef string, bucketName string) (bool, error)
 }
 
 type SimpleStore struct {
